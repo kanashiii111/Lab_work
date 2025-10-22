448f5654464d51c0eab8b7a395269b599ba957e9
kanashiii111 <FrageSK@yandex.ru>
2025-10-22 13:34:39 +0300

    fix

--- ProgrammingTech/lab2/js/server.js
+++ ProgrammingTech/lab2/js/server.js
@@ -1,37 +1,32 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-//инициализируем приложение
 const app = express()
 const port = 3000
-//подключаем статические файлы
 app.use(express.static(path.join(__dirname, '../css')))
 app.use(bodyParser.urlencoded({extended: true}));
 
-//создаем переменную для хранения данных
-const list = []
+orders = []
 
-//возвращаем данные по запросу
-app.get('/list', (req, res) => {
-    res.send(list)
+app.get('/orders', (req, res) => {
+    res.send(orders)
 })
 
-//выводим файл index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-//слушаем порт 3000
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })
 
-//добавляем в массив новые данные
 app.post('/', (req, res) => {
-
-    //передаем данные в массив
-    list.push(req.body)
-
-    //возвращаем ответ
-    res.send('Данные добавлены')
+    data = req.body
+    orders.push(data)
+    /*
+    console.log(`Матч: ${match}`)
+    console.log(`Билет: ${ticket}`)
+    console.log(`Кол-во билетов: ${quantity}`)
+    console.log(`Email: ${email}`)
+    */
 })
